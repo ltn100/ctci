c4dce3d598d7769cf68193d7c8885590ed87b953
Lee Netherton <ltn100@gmail.com>
2019-12-11 20:43:15 +0000

    Implemented 4.12

--- c4/4_12_paths_with_sum.py
+++ c4/4_12_paths_with_sum.py
@@ -1,6 +1,7 @@
 import pytest
 from contextlib import ExitStack as does_not_raise
 from graph import BinaryNode
+from collections import Counter
 
 
 class Solution:
@@ -10,10 +11,36 @@ class Solution:
         paths must only descend the tree, but can be from any node to any descendant
         node.
 
+        This solution does a DFS through the tree, keeping track of the cumulative sum
+        of the nodes since the root. The cumulative totals are insert into a hashmap. If
+        the difference between the current cumulative sum minus k equals any of the previously
+        calculated sums (in the hashmap) then there is a path to that previous node with total k.
+
         :param root: The root node of the tree
         :return: The path count
         """
-        return 5
+        hashmap_init = {0: 1}  # There will always be one path to the first node
+        return self.paths_with_sum_helper(root, k, Counter(hashmap_init), 0, 0)
+
+    def paths_with_sum_helper(self, node, k, hashmap, cumsum, paths):
+        if node is None:
+            return paths
+
+        cumsum += node.val
+        hashmap[cumsum] += 1
+
+        for part_sum, count in hashmap.items():
+            if part_sum == cumsum - k:
+                paths += count
+
+        # dfs
+        paths = self.paths_with_sum_helper(node.left, k, hashmap, cumsum, paths)
+        paths = self.paths_with_sum_helper(node.right, k, hashmap, cumsum, paths)
+
+        cumsum -= node.val
+        hashmap[cumsum] -= 1
+
+        return paths
 
 
 tree = node = BinaryNode(10)
@@ -22,18 +49,19 @@ node = node.left
 node.left = BinaryNode(1)
 node = node.left
 node.left = BinaryNode(2)
+node.right = BinaryNode(2)
 node = node.left
 node.left = BinaryNode(-1)
 node = node.leftnode = node.left
 node.left = BinaryNode(-1)
 node = node.left
-node.left = BinaryNode(7)
-node = node.left
+node.right = BinaryNode(7)
+node = node.right
 node.left = BinaryNode(1)
 node = node.left
 node.left = BinaryNode(2)
 testdata = [
-    ((tree, 8), 5, does_not_raise()),
+    ((tree, 8), 6, does_not_raise()),
 ]
 
 
